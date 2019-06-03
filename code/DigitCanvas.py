619cf9348abaa9918f6eb052c33a16160eae3b43
tomczak724 <sheerfox@gmail.com>
2019-06-02 21:09:07 -0400

    revised digit probability display

--- code/DigitCanvas.py
+++ code/DigitCanvas.py
@@ -21,19 +21,30 @@ class DigitCanvas(object):
 
     def __init__(self, image_size=28):
 
-        self.fig = pyplot.figure(figsize=(7,5))
-        self.ax_canvas = pyplot.subplot2grid((7,7), (0,0), rowspan=7, colspan=5, aspect='equal', label='canvas')
-        self.ax_clear = pyplot.subplot2grid((7,7), (0,5), rowspan=1, colspan=2, facecolor='#cccccc', label='clear button')
-        self.ax_submit = pyplot.subplot2grid((7,7), (1,5), rowspan=1, colspan=2, facecolor='#cccccc', label='submit button')
+        self.fig = pyplot.figure(figsize=(10,5))
+        self.ax_canvas = pyplot.subplot2grid((7,12), (0,0), rowspan=7, colspan=5, aspect='equal', label='canvas')
+        self.ax_clear = pyplot.subplot2grid((7,12), (0,5), rowspan=1, colspan=2, facecolor='#cccccc', label='clear button')
+        self.ax_submit = pyplot.subplot2grid((7,12), (1,5), rowspan=1, colspan=2, facecolor='#cccccc', label='submit button')
+        self.ax_probs = pyplot.subplot2grid((7,12), (3, 6), rowspan=4, colspan=6, label='probabilities')
         self.fig.subplots_adjust(left=0.03, right=0.97, hspace=0.4, wspace=0.2)
 
         self.neural_network = None
 
-        ###  turning off axis labels
+        ###  turning off axis labels for canvas and buttons
         for ax in [self.ax_canvas, self.ax_clear, self.ax_submit]:
             ax.xaxis.set_visible(False)
             ax.yaxis.set_visible(False)
 
+        ###  labeling probabilities panel
+        self.ax_probs.set_xlabel('Digit Label', size=14)
+        self.ax_probs.set_ylabel('Probability (%)', size=14)
+        self.ax_probs.minorticks_on()
+        self.ax_probs.set_ylim(-4, 112)
+        self.ax_probs.tick_params(axis='x', which='minor', bottom=False)
+
+        self.ax_probs.xaxis.set_ticks(range(10))
+        for i in numpy.arange(-0.5,10,1):
+            self.ax_probs.axvline(i, color='gray', lw=1, alpha=0.5)
 
         ###  initializing 2D image array
         self.image_size = image_size
@@ -48,9 +59,10 @@ class DigitCanvas(object):
 
         ###  adding button labels
         self.ax_canvas.set_title('Draw a single-digit number', size=14)
-        self.ax_clear.text(0.5, 0.5, 'CLEAR', fontweight='bold', size=14, ha='center', va='center')
-        self.ax_submit.text(0.5, 0.5, 'SUBMIT', fontweight='bold', size=14, ha='center', va='center')
+        self.ax_clear.text(0.5, 0.5, 'CLEAR', fontweight='bold', family='sans-serif', size=14, ha='center', va='center')
+        self.ax_submit.text(0.5, 0.5, 'SUBMIT', fontweight='bold', family='sans-serif', size=14, ha='center', va='center')
 
+        self.last_panel_clicked = None
         self.mouse_depressed = False
         self.sketch_coords = {'x':[], 'y':[]}
         self.sketch_collection = []
@@ -62,8 +74,9 @@ class DigitCanvas(object):
 
         self.sketch_plot = self.ax_canvas.plot(0, 0, color='k', lw=10)[0]
         self.image_plot = self.ax_canvas.imshow(self.image_data, interpolation='none', vmin=0, vmax=1, cmap=pyplot.cm.Greens)
-        self.text_probs_plot = self.ax_canvas.text(1.1, 0.05, '', size=12, transform=self.ax_canvas.transAxes)
-        self.text_guess_plot = self.ax_canvas.text(0.5, 0.98, '', size=16, transform=self.ax_canvas.transAxes, ha='center', va='top', fontweight='bold', family='sans-serif', color='r')
+        self.pdf_curve = self.ax_probs.step(range(-1,11), numpy.zeros(12)-100, where='mid', color='#1f77b4', lw=2)[0]
+        self.text_probs_plot = [self.ax_probs.text(x, -10, '', size=12, ha='center', va='bottom',  color='#1f77b4', fontweight='bold', family='sans-serif') for x in range(10)]
+
 
     def _onClick(self, event):
 
@@ -81,12 +94,15 @@ class DigitCanvas(object):
             self.image_data = numpy.zeros((self.image_size, self.image_size))
             self.image_plot.set_data(self.image_data)
 
-            self.text_probs_plot.set_text('')
-            self.text_guess_plot.set_text('')
+            self.pdf_curve.set_ydata(numpy.zeros(12)-100)
+            for t in self.text_probs_plot:
+                t.set_text('')
+
+            self.ax_probs.set_title('')
             pyplot.draw()
 
         ###  handling SUBMIT button click
-        if (event.inaxes is not None) and (event.inaxes.get_label() == 'submit button'):
+        if (event.inaxes is not None) and (event.inaxes.get_label() == 'submit button') and (self.last_panel_clicked != 'submit button'):
 
             xdata = numpy.array(self.sketch_coords['x']).astype(float)
             ydata = numpy.array(self.sketch_coords['y']).astype(float)
@@ -129,24 +145,34 @@ class DigitCanvas(object):
 
             if self.neural_network is not None:
 
-                ###  inverting image along y-axis (MNIST data format)
+                ###  inverting image along y-axis and flattening (MNIST data format)
                 im = self.image_data[::-1, :].flatten()
+
+                ###  guessing and getting probabilities
                 guess = self.neural_network.guess_digit(im)
+                probs = 100 * self.neural_network.get_probabilities(im)
+
+                ###  plotting PDF curve and probabilities text
+                self.pdf_curve.set_ydata([0]+probs.tolist()+[0])
+                for i in range(10):
+                    t = self.text_probs_plot[i]
+                    t.set_text('%i' % probs[i])
+                    t.set_y(probs[i])
 
                 ###  indicating best guess
-                text1 = 'Looks like a %i to me!' % guess
-                self.text_guess_plot.set_text(text1)
+                if probs.max() > 80:
+                    title = 'Looks like a %i to me!' % guess
+                elif probs.max() > 70:
+                    title = 'Looks mostly like a %i to me' % guess
+                elif probs.max() > 60:
+                    title = 'Looks kind of like a %i to me' % guess
+                else:
+                    title = 'I\'m not really sure, but I guess %i?' % guess
 
-                ###  getting probabilities
-                zs, activations = self.neural_network._forward_propagate(im)
-                probs = activations[-1] / activations[-1].sum()
+                self.ax_probs.set_title(title, color='r', fontweight='bold', family='sans-serif', size=14)
 
-                text = 'Digit probabilities\n'
-                for i, p in enumerate(probs):
-                    text += '\n    %i  =  %.1f %%' % (i, p*100)
-
-                self.text_probs_plot.set_text(text)
-
+        if (event.inaxes is not None):
+            self.last_panel_clicked = event.inaxes.get_label()
 
     def _onClickRelease(self, event):
         self.mouse_depressed = False
